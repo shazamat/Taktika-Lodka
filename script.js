3adb640487e185fcdf36ee9aad116e1c2a4fb944
shomuratov <shomuratovazamat116@gmail.com>
2024-02-20 23:43:51 +0500

    main section

---
+++ script.js
@@ -0,0 +1,23 @@
+const baza = {
+    "data": {
+        "bookings": {
+            "previous": 0,
+            "current": 91
+        },
+        "clients": {
+            "previous": 32,
+            "current": 32
+        },
+        "drivers": {
+            "previous": 7,
+            "current": 7
+        },
+        "incomes": {
+            "previous": "750",
+            "current": "1500"
+        }
+    },
+}
+
+
+console.log(baza.data.bookings);
